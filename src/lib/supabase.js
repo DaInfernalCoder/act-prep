35bcf8d2aecc097bcc0e0fa177446ae62093a858
Sumit Datta <111019954+DaInfernalCoder@users.noreply.github.com>
2026-05-16 22:04:26 -0500

    migrate to new supabase project, add auth, 13 tests

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://pnuihxemmmtdpvakndgg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBudWloeGVtbW10ZHB2YWtuZGdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMTYwOTYsImV4cCI6MjA4OTg5MjA5Nn0.rwDVpFvD7GHQy0GUpaF0dwXxdVSlf20zQ0GD24OR7tY'
+  'https://ylgxnutrbejubgiqcsew.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsZ3hudXRyYmVqdWJnaXFjc2V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NzIwMzEsImV4cCI6MjA5NDU0ODAzMX0.hrfHf8MxEBYsdA90BSF8QzUJrcPdFFCT-JVJusz5jY4'
 )
